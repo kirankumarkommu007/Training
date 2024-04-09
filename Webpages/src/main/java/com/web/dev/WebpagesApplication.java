6871e206e20e4c7ec3aa8ac29486c4dc66bf4d63
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-04-09 13:39:34 +0530

    added web pages

---
+++ Webpages/src/main/java/com/web/dev/WebpagesApplication.java
@@ -0,0 +1,13 @@
+package com.web.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpagesApplication.class, args);
+	}
+
+}
